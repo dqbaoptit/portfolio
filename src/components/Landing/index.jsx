7282365d59581eecee2e9c34ab0fba50ee5ac7de
Bao <dqbaoptit@gmail.com>
2020-06-12 09:30:45 +0700

    added media query for landing content

--- src/components/Landing/index.jsx
+++ src/components/Landing/index.jsx
@@ -13,7 +13,7 @@ import './index.css'
 				<AnchorLink href={props.to}>
 					<div className="landing-content" align="center">
 						<button className="button">
-							<h3>Hi, I'm <strong style={{fontSize:30}}>Quoc Bao</strong>.</h3>
+								<h3>Hi, I'm <strong style={{fontSize:30}}>Quoc Bao</strong>.</h3>
 							<p style={{color:"#fff"}}>I'm  a</p> <h3>Front-end Developer.</h3>
 							<div className="button__horizontal"></div>
 							<div className="button__vertical"></div>
